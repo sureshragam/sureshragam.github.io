6eb19341fd69631a66b60a7d7cee3923dd18d60d
suresh <suresh.ragam@icloud.com>
2024-11-06 19:07:45 +0530

    changes in certificate section

--- src/components/Certificates/certificates.tsx
+++ src/components/Certificates/certificates.tsx
@@ -1,40 +1,28 @@
 import React from "react"
+import {v4 as uuidv4} from 'uuid'
 import classes from './certificates.module.css'
 
+function Certificate (name:string,description:string,url:string,id:string) {  //Object constructor function
+    this.name=name;
+    this.description=description;
+    this.url=url;
+    this.id=id
+}
+
 const certificatesData = [
-    {name:'HTML & CSS',
-        description:"some content",
-        url:""
-    },
-    {name:'Python',
-        description:"some content",
-        url:""
-    },
-    {name:'AWS',
-        description:"some content",
-        url:""
-    },
-    {name:'HTML & CSS',
-        description:"some content",
-        url:""
-    },
-    {name:'Python',
-        description:"some content",
-        url:""
-    },
-    {name:'AWS',
-        description:"some content",
-        url:""
-    },
+    new Certificate("React","someting","",uuidv4()),
+    new Certificate("HTML","someting","",uuidv4()),
+    new Certificate("CSS","someting","",uuidv4()),
+    new Certificate("JAVASCRIPT","someting","",uuidv4())
 ]
 
-const Certificates = () =>{
+const Certificates:React.FC = () =>{
     return (
         <div id="certificates" className={classes.certificatesContainer}>
             <h1 className={classes.title}>My Certificates</h1>
             <ul className={classes.certificates}>
                 {certificatesData.map(eachCertificate =>{
-                    return <li className={classes.certificate}>
+                    return <li className={classes.certificate} key={eachCertificate.id}>
                         <h1>{eachCertificate.name}</h1>
                         <p>{eachCertificate.description}</p>
                     </li>
